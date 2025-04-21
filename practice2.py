daac5b23a39581c92af874f3957f74d626d74235
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-04-21 13:53:40 +0530

    crud in Chroma

--- practice2.py
+++ practice2.py
@@ -1,10 +1,15 @@
+# goal learn CRUD in chroma
+
 import chromadb
 from chromadb.config import Settings
 client = chromadb.Client()
 client = chromadb.PersistentClient(path="./chroma_db")
 
+
 collection = client.get_or_create_collection(name = "my_collection")
 
+
+# CREATE
 collection.add(
     documents=[
         "The sun rises in the east.",
@@ -23,6 +28,41 @@ collection.add(
     ]
 )
 
-result = collection.get(ids=["doc1"])
+#READ
+#By ID 
+result1 = collection.get(ids=["doc1"])
+#By metadata
+result2 = collection.get(where={"topic":"nature"})
+#By semantic similarity
+result3 = collection.query(query_texts=["pets"],n_results=1)
+
+
+
+
+#UPDATE 
+
+
+collection.upsert(
+    documents=["Cats are lovely companions"],
+    ids=["doc2"],
+    metadatas=[{"topic": "pets"}]
+)
+
+
+
+#DELETE
+
+# Delete specific doc
+collection.delete(ids=["doc3"])
+
+# Delete by metadata
+collection.delete(where={"topic": "technology"})
+
+# OR delete entire collection
 client.delete_collection(name="my_collection")
-print(result)
+
+
+
+# print(result1)
+# print(result2)
+print(result3)
